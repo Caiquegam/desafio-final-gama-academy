726a3552a6d753d2ba68a6ecdf9900b3a6d4be76
Caiquegam <caiquegamadeoliveira@gmail.com>
2023-02-11 16:22:39 -0300

    criacao de carrocel

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import Header from "./components/Header";
+import "./App.css";
+import Carousel from "./components/FolderCarousel/Carousel"
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <h1>Pagina Home</h1>
+        <Header />
+        <h1>Pagina Home</h1>
+    <div>
+      <Carousel/>
     </div>
-  );
+    </div>
+  )
 }
-
-export default App;
+export default App
